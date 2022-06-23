3a719f2f2265f4cada8c3f10f5c93dd869c0cee9
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-06-23 18:27:53 -0500

    added variables and mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const MongoClient= require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 require('dontenv').config()
 
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'eyeOfTheWorld',
+    collection 
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection =db.collection('characters')
+    })
+
+ // ejs require not setup yet
